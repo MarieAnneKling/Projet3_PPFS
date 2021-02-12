eba0e5d8fac4fb80923057d1317a0ab0678e3ba7
Marie-Anne Kling <marieannekling@gmail.com>
2021-02-12 15:17:59 +0100

    page acteur commentée

--- pages/page_acteur.php
+++ pages/page_acteur.php
@@ -4,108 +4,97 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="..\styles\style.css" />
-    <head>
-        <?php include("..\communs\head.php");?>
-    </head>
-
-    <body>   
-        
-       
-       
-        <?php require ("..\communs\bdd_gbaf.php");?>     
-        
-        <?php include("..\communs\header.php");?>
-                      
-        <section id="actor_presentation">
-            <div class= "actor">
-           
-            <?php
-            if (isset($_GET['id_actor']))
-            {
-            $_SESSION['id_actor'] = $_GET['id_actor'];
-            $id_actor=$_SESSION['id_actor'];
-            $req=$bdd->prepare('SELECT * FROM actors WHERE id_actor = :id_actor');
-            $req->execute(array('id_actor'=>$id_actor));
-            $donnees=$req->fetch();
-
-            echo '<img class="img_actor" src='.$donnees["logo"].'>';
-            echo '<h2>' .$donnees["actor_name"]. '</h2>' ; ?>
-            <div class="actor_description"><?php echo '<p>' .$donnees["description"]. '</p></div>'; 
-            }
-            ?> 
-        </section>
-          
-        <section id="votes">
-           <!--Total des votes pour l'acteur--> 
-           <?php
-           $req = $bdd->prepare('SELECT COUNT(*) AS nb_likes FROM votes WHERE id_actor = :id_actor && vote = 1');
-           $req->execute(array(
-          'id_actor'=>$id_actor
-           ));
-           $nb_likes = $req->fetch();
-          
-          $req = $bdd->prepare('SELECT COUNT(*) AS nb_likes FROM votes WHERE id_actor = :id_actor && vote = 0');
-          $req->execute(array(
-         'id_actor'=>$id_actor
-          ));
-          $nb_dislikes = $req->fetch();
-          ?>
-          <div class="nb_votes">
-          <button class="nb_likes"><i class="far fa-thumbs-up"></i></a><?php echo $nb_likes['nb_likes'];?> 
-          </button>     
-          <button class="nb_dislikes"><i class="far fa-thumbs-down"></i></a><?php echo $nb_dislikes['nb_likes'];?></button>
-          </div>
-
+<link rel="stylesheet" href="..\styles\style.css">
+  <head>
+      <!--Appel du fichier contenant les informations générales non affichées de la page--> 
+      <?php include("..\communs\head.php");?>
+  </head>
+ 
+  <body>   
+    <!--Appel de la base de données-->
+    <?php require ("..\communs\bdd_gbaf.php");?>     
       
-         
-         </section></br>
+    <!--Appel du fichier contenant l'en-tête-->  
+    <?php include("..\communs\header.php");?>
+      
+    <!-- début de la partie présentation des acteurs-->          
+    <section id="actor_presentation">
+            
+      <!--Liste des acteurs obtenue grâce à une requête sur la base de données--> 
+      <div class= "actor">
+      <?php
+      if (isset($_GET['id_actor']))
+      {
+        $_SESSION['id_actor'] = $_GET['id_actor'];
+        $id_actor=$_SESSION['id_actor'];
+        $req=$bdd->prepare('SELECT * FROM actors WHERE id_actor = :id_actor');
+        $req->execute(array('id_actor'=>$id_actor));
+        $donnees=$req->fetch();
+        echo '<img class="img_actor" src='.$donnees["logo"].'>';
+        echo '<h2>' .$donnees["actor_name"]. '</h2>' ; ?>
+      <div class="actor_description"><?php echo '<p>' .$donnees["description"]. '</p></div>'; 
+      }
+      ?> 
+    </section>
+          
+    <!--Affichage du total des votes likes et dislikes pour l'acteur grâce à une requête sur la base de données--> 
+    <section id="votes">
+      <?php
+      $req = $bdd->prepare('SELECT COUNT(*) AS nb_likes FROM votes WHERE id_actor = :id_actor && vote = 1');
+      $req->execute(array(
+        'id_actor'=>$id_actor
+        ));
+      $nb_likes = $req->fetch();
+          
+      $req = $bdd->prepare('SELECT COUNT(*) AS nb_likes FROM votes WHERE id_actor = :id_actor && vote = 0');
+      $req->execute(array(
+        'id_actor'=>$id_actor
+        ));
+      $nb_dislikes = $req->fetch();
+      ?>
+      <div class="nb_votes">
+        <button class="nb_likes"><i class="far fa-thumbs-up"></i></a><?php echo $nb_likes['nb_likes'];?></button>     
+        <button class="nb_dislikes"><i class="far fa-thumbs-down"></i></a><?php echo $nb_dislikes['nb_likes'];?></button>
+      </div>
+    </section></br>
                    
-         <!--insertion d'un commentaire à l'aide d'un formulaire-->
+      <!--insertion d'un commentaire pour cet acteur à l'aide d'un formulaire-->
         <section id="insert_comment">
           <form action="..\processus\insert_comment.php" method="post">                
-          <div class="post"><label>Votre commentaire sur cet acteur<input type="text" placeholder="Saisissez votre commentaire" name="post" class="champ_saisie"></label></div>
-          <p><input type="submit" class="button" value="Validez votre commentaire"/></a></p>
+            <div class="post"><label>Votre commentaire sur cet acteur<input type="text" placeholder="Saisissez votre commentaire" name="post" class="champ_saisie"></label></div>
+            <p><input type="submit" class="button" value="Validez votre commentaire"/></a></p>
+            </div>
           </form>
-          </div>
         </section>
 
-        <section id="vote">
-           
-           <!--Insertion des votes pour cet acteur-->          
-              <div class="vote_btns">
-              <a href="..\processus\insert_votes.php?id_user=$id_user&id_actor=$id_actor&vote=1">Votez pour cet acteur
-              <button class="vote_btn vote_like"><i class="far fa-thumbs-up"></i></a><?php echo "";?> 
-              </button>
-                       
-              <a href="..\processus\insert_votes.php?id_user=$id_user&id_actor=$id_actor&vote=0">
-              <button class="vote_btn vote_dislike"><i class="far fa-thumbs-down"></i></a><?php echo "";?></button>
-              </div>
-         </section><br> 
+      <!--Insertion du vote pour cet acteur en cliquant sur les boutons likes et dilslikes-->  
+      <section id="vote">
+        <div class="vote_btns">
+          <a href="..\processus\insert_votes.php?id_user=$id_user&id_actor=$id_actor&vote=1">Votez pour cet acteur
+          <button class="vote_btn vote_like"><i class="far fa-thumbs-up"></i></a><?php echo "";?> 
+          </button>          
+          <a href="..\processus\insert_votes.php?id_user=$id_user&id_actor=$id_actor&vote=0">
+          <button class="vote_btn vote_dislike"><i class="far fa-thumbs-down"></i></a><?php echo "";?></button>
+        </div>
+      </section><br> 
          
-         <!--affichage des 10 dernierscommentaires-->
+        <!--affichage des 10 derniers commentaires pour cet acteur grâce à une requête sur la base de données-->
         <section id="view_comment">
           <?php
-            $req = $bdd->prepare('SELECT * FROM posts NATURAL JOIN account WHERE id_actor = :id_actor ORDER BY id_user DESC LIMIT 0, 10');
-            $req->execute(array(
-              'id_actor'=> $id_actor
+          $req = $bdd->prepare('SELECT * FROM posts NATURAL JOIN account WHERE id_actor = :id_actor ORDER BY id_user DESC LIMIT 0, 10');
+          $req->execute(array(
+            'id_actor'=> $id_actor
             ));
-            while ($donnees = $req->fetch())
-            {
+          while ($donnees = $req->fetch())
+          {
             echo '<div class=view_comm><div class="forname">Posté par '.' '.$donnees['forname'].' '.'le'.' '.$donnees['date_add'].'</div>'; 
-            
             echo '<div class="comment">'.nl2br(htmlspecialchars($donnees['post'])).'</div></div>';  
-            }   
-            ?>
-          </section>  
+          }   
+          ?>
+        </section>  
           
-                    
-
- 
-        
-        
+        <!--Appel du fichier contenant le pied de page-->   
         <?php include("..\communs\Footer.php");?>
           
     </body>
 </html>
-
